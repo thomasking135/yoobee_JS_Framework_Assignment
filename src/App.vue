ba20a0fa5d11a81d0b3c48629d781987d4b6f387
justinaho <cyjustinaho@gmail.com>
2022-08-26 16:36:05 +1200

    SPA set up

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template> 
+  <app-header /> 
+  <router-view/>
+  <app-footer />  
+</template> 
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'  
+
+export default { 
+  components: { 
+    AppHeader,
+    AppFooter  
+  } 
+} 
+</script> 
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
