aa15107f9be13bdff169439673e523bc37e53a71
thomasking135 <thomasking135@gmail.com>
2022-08-29 17:28:14 +1200

    Created images folder and placed pictures inside

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <router-link to="/"><img alt="Vue logo" src="./assets/logo.png" ></router-link><nav>
+  <router-link to="/"><img alt="Vue logo" src="./assets/images/logoNoText.png" ></router-link><nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>|  
     <router-link to="/profile">Profile</router-link>|
