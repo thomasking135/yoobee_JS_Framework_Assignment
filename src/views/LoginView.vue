3b3a6fbad3c802bc3c350b538bdb68239b7f75fd
justinaho <cyjustinaho@gmail.com>
2022-08-30 14:15:05 +1200

    Added files and started on login/logoff page.
    Added images/icons/logo files.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,22 +1,46 @@
 <template>
-
-  <div class="login">
-    
-  <h1>Login</h1>  
-    <p>Insert login details</p>
+  <div class="background-image" :style="{ backgroundImage: 'url(' + backgroundUrl + ')' }">
+    <div class="login">
+    <h1>Login</h1>  
+      <p>Insert login details</p>
+    </div>
   </div>
 </template>
 
 
 
 <style>
+* {
+margin: 0;
+padding: 0;
+}
+
 h1{
   text-align: center;
 }
+
 .login {
   padding-top: 100px;
   text-align: justify;
   margin-left: 25%;
   margin-right: 25%;
 }
+
+.background-image {
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+}
 </style>
+
+<script>
+import backgroundUrl from "../assets/images/LogInSignUpDesktop.jpg";
+
+export default {
+  data() {
+    return {
+      backgroundUrl,
+    };
+  },
+};
+</script>
