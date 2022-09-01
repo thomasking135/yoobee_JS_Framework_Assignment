2ab9445d0d89cf10129e2134fa444da786ce35ab
KeeganDufty <keegz_d@icloud.com>
2022-09-02 06:39:40 +1200

    Update to Profile Page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,16 +1,62 @@
 <template>
-
   <div class="profile">
-    
-  <h1>Profile</h1>  
-    <p>Insert profile details</p>
+    <div class="profileGrid">
+      <div class="ProfileImageContainer">
+        <div class="ProfileImage"></div>
+        <button class="updateProfileImage">Update Image</button>
+      </div>
+      <div class="profileDetails">
+        <input type="text" placeholder="User Name" />
+        <input type="text" placeholder="Bio" />
+        <button type="submit" class="submitBio">Update</button>
+        <button type="submit" class="LogOutBtn">Log Out</button>
+      </div>
+    </div>
   </div>
 </template>
 
-
-
 <style>
-h1{
+.ProfileImageContainer {
+  display: flex;
+  flex-direction: column;
+  margin-top: 100px;
+}
+
+.ProfileImage {
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  border: 1px solid black;
+  margin-bottom: 20px;
+}
+
+.updateProfileImage {
+  width: 100px;
+  background-color: white;
+  border: 1px solid #06beb6;
+  padding: 10px;
+}
+
+.profileDetails {
+  display: flex;
+  flex-direction: column;
+  margin-top: 100px;
+}
+
+.submitBio {
+  background-color: white;
+  border: 1px solid #06beb6;
+  padding: 10px;
+  width: 30%;
+}
+.LogOutBtn {
+  background-color: #06beb6;
+  border: 1px solid #06beb6;
+  color: #303030;
+  padding: 10px;
+  width: 30%;
+}
+h1 {
   text-align: center;
 }
 .profile {
