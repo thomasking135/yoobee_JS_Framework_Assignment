d0412d6ed1a55e4124f242cace9df5a39f0090fd
thomasking135 <thomasking135@gmail.com>
2022-09-02 17:54:14 +1200

    Update of create post page.

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -2,7 +2,7 @@
 
     <div class="create">
       
-    <h1>Create Event</h1>  
+    <h1 class="createEventHeader">Create Event</h1>  
     
       <div class="centerMessaging">
         <img class="float" src="../assets/icons/uploadImage.png" alt="upload" >
@@ -15,48 +15,5 @@
   
   
   <style>
-  h1{
-    text-align: center;
-    color: black;
-    padding: 10%;
-  }
-
-  .float {
-    float: right;
-  }
-    
-  
-  .create { 
-    margin-left: auto;
-    margin-right: auto;
-    color: black;
-    text-align: center;
-    padding: 2%;
-
-
-  }
-
-  .imageUpload{
-    margin-left: auto;
-    margin-right: auto;
-    position:justify;
-    color:white;
-    text-align: auto;
-    
-  }
-  .centerMessaging {
-  padding: 40px;
-  position: absolute;
-  top: 45%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: white ;
-  font-size: 200%;
-  font-weight: bold;
-  background-color: lightgray;
-  width: 100%;
-  height: 13%;
-  text-align: center;
-  
-}
+  @import '../assets/styles/main.css';
   </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,47 +12,7 @@
 
 <style>
 
-.container {
-  padding-top: -100px;
-  position: absolute;
-  text-align: center;
-  color: white;
-}
 
-.centerMessage {
-  position: absolute;
-  top: 45%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: palegoldenrod ;
-  font-size: 300%;
-  font-weight: bold;
-}
-
-.createEvent {
-  background-color: #08BDB6;
-  text-align: center;
-  font-size: 14px;
-  font-family: lato, sans-serif;
-  font-weight: 500;
-  font-style: normal;
-  color: #303030;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 50px;
-  padding-right: 50px;
- 
-}
-
-.createEvent:hover {
-  background-color: #48B1BF;
-  transform: scale(1.1);
-  border-radius: 26%;
-}
-
-.home {
-  padding-top: 200px;
-}
 
 
 </style>
