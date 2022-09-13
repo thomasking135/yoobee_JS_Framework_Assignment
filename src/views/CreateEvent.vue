dba7047ecca6a20f914e99c02bec919adb716492
thomasking135 <thomasking135@gmail.com>
2022-09-13 16:40:31 +1200

    Update of refreshed pages
    Adding of refreshed data for vues, components, and templates

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -10,7 +10,31 @@
         <br><br><p class="imageUpload">Click to upload image</p>
       </div>
 
-      <form >
+      <div id="employee-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Employee name</label>
+      <input
+        ref="first"
+        type="text"
+        :class="{ 'has-error': submitting && invalidName }"
+        v-model="employee.name"
+        @focus="clearStatus"
+        @keypress="clearStatus"
+      >
+      <label>Employee Email</label>
+      <input
+        type="text"
+        :class="{ 'has-error': submitting && invalidEmail }"
+        v-model="employee.email"
+        @focus="clearStatus"
+      >
+      <p v-if="error && submitting" class="error-message">❗Please fill out all required fields</p>
+      <p v-if="success" class="success-message">✅ Employee successfully added</p>
+      <button>Add Employee</button>
+    </form>
+  </div>
+      <!--<form >
+        
   <div class="formBorder">
   <label  for="date">Starts on:</label><br>
   <input type="date" id="sdate" name="sdate" value="Start Date"><br>
@@ -44,7 +68,7 @@
     <br><br><button class="postit">Post Event</button>
     <button class="cancelit">Cancel</button>
   </div>  
-</form> 
+</form>--> 
 
     </div>
 
@@ -52,7 +76,56 @@
   
   </template>
   
-  
+  <script>
+    export default {
+      name: "employee-form",
+      data() {
+        return {
+          error: false,
+          submitting: false,
+          success: false,
+          employee: {
+            name: "",
+            email: ""
+          }
+        };
+      },
+      computed: {
+        invalidName() {
+          return this.employee.name === "";
+        },
+    
+        invalidEmail() {
+          return this.employee.email === "";
+        }
+      },
+      methods: {
+        handleSubmit() {
+          this.clearStatus();
+          this.submitting = true;
+    
+          if (this.invalidName || this.invalidEmail) {
+            this.error = true;
+            return;
+          }
+    
+          this.$emit("add:employee", this.employee);
+          this.$refs.first.focus();
+          this.employee = {
+            name: "",
+            email: ""
+          };
+          this.clearStatus();
+          this.submitting = false;
+        },
+    
+        clearStatus() {
+          this.success = false;
+          this.error = false;
+        }
+      }
+    };
+    </script>
   
   <style>
   @import '../assets/styles/main.css';
