ae39561782f1091107ed197634ebd3761336d53a
thomasking135 <thomasking135@gmail.com>
2022-09-17 19:27:22 +1200

    Update to profile page

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,6 +1,6 @@
 <template> 
 <br><br>
-    <a href="https://www.google.com"><img class="gitHub" src="../assets/icons/githubIcon.png"></a>
+    <a href="https://thomasking135.github.io/yoobee.github.io/"><img class="gitHub" src="../assets/icons/githubIcon.png"></a>
     <h3>Ⓒ2022</h3>
 </template>
 
