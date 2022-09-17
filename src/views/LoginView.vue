7656eaba49a9f96f847eebea8c45e25911a53a1b
justinaho <cyjustinaho@gmail.com>
2022-09-17 21:06:46 +1200

    Fixed some errors with login
    Still have some errors such as mixed matched login details still letting you through if they are in the JSON file already. Need to make it so both inputs have to be the same person's details to be able to login.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -24,112 +24,52 @@
             v-model.trim="username"
             required
           />
-          
+
           <button type="submit" id="submitLogin">Login</button>
         </div>
 
         <div class="loginForm__container">
-          <span class="signUp"
-            ><a href="/register">Don't have an account?</a></span
-          >
+          <span class="signUp"><a href="/register">Don't have an account?</a></span>
         </div>
-        <br><br>
-        <div class="centering">
+
         <div class="loginForm__container">
-          <span class="signUp"
-            ><div class="tooltip">Need answers?
-        <span class="tooltiptext"><router-link to="/about">Click here to learn more</router-link></span>
-        </div></span
-          >
+          <span class="learnMore"><a href="/about">Learn more!</a></span>
         </div>
-        </div>
+
       </form>
     </div>
+
     <div v-if="!formIsValid">Please enter a valid email and username</div>
     <div v-if="!userDetailsIsValid">Email and/or username is incorrect</div>
 
   </div>
-
-  
-
 </template>
-
-
 
 <style>
 @import "../assets/styles/main.css";
-
-.tooltip {
-  position: relative;
-  display: inline-block;
-  border-bottom: 1px dotted blue;
-  margin-left:auto;
-  margin-right: auto;
-  font-weight: bold;
-}
-
-.tooltip .tooltiptext {
-  visibility: hidden;
-  width: 120px;
-  background-color: #555;
-  color: #fff;
-  text-align: center;
-  border-radius: 6px;
-  padding: 5px 0;
-  position: absolute;
-  z-index: 1;
-  bottom: 125%;
-  left: 50%;
-  margin-left: -60px;
-  opacity: 0;
-  transition: opacity 0.3s;
-  float: right;
-}
-
-.tooltip .tooltiptext::after {
-  content: "";
-  position: absolute;
-  top: 100%;
-  left: 50%;
-  margin-left: -5px;
-  border-width: 5px;
-  border-style: solid;
-  border-color: #555 transparent transparent transparent;
-}
-
-.tooltip:hover .tooltiptext {
-  visibility: visible;
-  opacity: 1;
-}
-
-.centering{
-  margin-left: auto;
-  margin-right: auto;
-  float: right;
-}
 </style>
 
 <script>
 import backgroundUrl from "../assets/images/LogInSignUpDesktop.jpg";
 import axios from "axios";
-const baseURL = "http://localhost:3000/users"
+const baseURL = "http://localhost:3000/users";
 export default {
-  name: 'login',
+  name: "login",
   data() {
     return {
       backgroundUrl,
       email: "",
       username: "",
       users: [],
-      url: 'http://localhost:3000/users',
+      url: "http://localhost:3000/users",
       formIsValid: true,
-      userDetailsIsValid: true
+      userDetailsIsValid: true,
     };
   },
   mounted() {
     axios.get(this.url).then((response) => {
-      this.users = response.data
-    })
+      this.users = response.data;
+    });
   },
   methods: {
     submitForm() {
@@ -143,15 +83,17 @@ export default {
         return;
       }
 
-      for (let i=0; i<=this.users.length; i++) {
-        if (this.username == this.users[i].username){
-          console.log('yay');
+      for (let i = 0; i <= this.users.length; i++) {
+        if (this.username == this.users[i].username) {
           window.location.href = "/";
         }
         if (this.email == this.users[i].email) {
           window.location.href = "/";
         }
-      }
+        if (this.username !== this.users.username) {
+           this.userDetailsIsValid = false;
+        }
+      } 
 
       //GET
       axios
