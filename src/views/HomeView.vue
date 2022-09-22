b28bb2d525af1645cad1f843daa079f054817174
KeeganDufty <keegz_d@icloud.com>
2022-09-22 14:51:23 +1200

    Home page update, Profile page updates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <!--Thomas Code-->
 <!--Template-->
 <template>
- <div class="centerMessage">
-    <h2>  Kia ora, Hap is where it's at! </h2>
-      <router-link to="/create"
-        ><button class="button--landing">
-          Create a new event
-        </button></router-link
-      >
+  <div class="centerMessage">
+    <h2>Kia ora, Hap is where it's at!</h2>
+    <router-link to="/create"
+      ><button class="button--landing">Create a new event</button></router-link
+    >
   </div>
   <div class="overlayLandingImg">
     <img
@@ -16,6 +14,14 @@
       alt="Landing page"
     />
   </div>
+
+  <event-table
+    class="displayCenter"
+    :events="events"
+    @delete:event="deleteEvent"
+    @edit:event="editEvent"
+  />
+  <FooterSection />
 
   <!-- <div class="accordion">
     <form action="/create">
@@ -33,11 +39,48 @@
 
       <button class="search button--secondary">Search</button>
       <br /> -->
-    <!-- </form> -->
+  <!-- </form> -->
   <!-- </div> -->
 </template>
 <!--End of template-->
+<script>
+// eslint-disable-next-line no-unused-vars
+import Axios from "axios";
+import FooterSection from "@/components/FooterSection.vue";
+import EventTable from "@/components/EventTable.vue";
+const baseURLEvents = "http://localhost:3000/events";
 
+export default {
+  name: "App",
+  components: {
+    FooterSection,
+    EventTable,
+    // eslint-disable-next-line vue/no-unused-components
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  mounted() {
+    this.getEvents();
+  },
+  methods: {
+    async getEvents() {
+      try {
+        const response = await fetch(
+          baseURLEvents
+          /*Use of the JSON https API created by Justina*/
+        );
+        const data = await response.json();
+        this.events = data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
 <!--Style section-->
 <style>
 /*
